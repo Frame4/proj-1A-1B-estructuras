0d9484c19637377e76d5d64cce2fce2eaefeaaf0
Marco Angulo <marcotgp104@gmail.com>
2023-10-12 17:42:31 -0600

    first commit

---
+++ 1B/proyecto 1B/ListaSimple.h
@@ -0,0 +1,241 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+using namespace std;
+//******************************************
+typedef struct TArticulo
+{	
+	char cedula[10];
+	char codelec[7];
+	char sexo;
+	char fecha[9];
+	char numjun[6];
+	char nombre[31];
+	char papellido[27];
+	char sapellido[27];
+	/*
+	int Codigo;
+	char Nombre[20];
+	int Disponible;
+	float Precio;
+	*/
+	TArticulo *Siguiente;
+}*PtrTArticulo;
+
+
+//******************************************
+
+int i;  //parametro global
+
+
+void InicializarInventario(PtrTArticulo &Lista)
+{
+	Lista = NULL;
+}
+void DestruirInventario(PtrTArticulo &Lista)
+{
+	PtrTArticulo Aux;
+	Aux = Lista;
+	while (Aux != NULL)
+	{
+		Lista = Lista->Siguiente;
+		delete(Aux);
+		Aux = Lista;
+	}
+}
+/*
+PtrTArticulo CrearArticulo(int NCodigo, int NDisponible, float NPrecio)
+{
+	PtrTArticulo Pieza = new(TArticulo);
+	char buffer[5];
+
+	Pieza->Codigo = NCodigo;
+	Pieza->Disponible = NDisponible;
+	Pieza->Precio = NPrecio;
+
+	strcpy_s(Pieza->Nombre, "Pieza");
+	_itoa_s(NCodigo, buffer, 10);
+	strcat_s(Pieza->Nombre, buffer);
+
+	Pieza->Siguiente = NULL;
+	return Pieza;
+}
+*/
+void AgregarInicioInventario(PtrTArticulo &Lista, PtrTArticulo &Nuevo)
+{
+	Nuevo->Siguiente = Lista;
+	Lista = Nuevo;
+}
+void AgregarFinalInventario(PtrTArticulo &Lista, PtrTArticulo &Nuevo)
+{
+	PtrTArticulo Aux;
+	Aux = Lista;
+	if (Aux != NULL)
+	{
+		while (Aux->Siguiente != NULL)
+		{
+			Aux = Aux->Siguiente;
+		}
+
+		Aux->Siguiente = Nuevo;
+	}
+	else
+	{
+		Lista = Nuevo;
+	}
+
+
+}
+void ListarInventario(PtrTArticulo &Lista)
+{
+	int Contador = 1;
+	PtrTArticulo Aux;
+	Aux = Lista;
+	while (Aux != NULL)
+	{
+		cout << "Ce'dula: " << Aux->cedula << endl;
+		cout << "Nombre: " << Aux->nombre << endl;
+		cout << "Primer Apellido: " << Aux->papellido << endl;
+		cout << "Segundo Apellido: " << Aux->sapellido << endl;
+		cout << "Sexo: " << Aux->sexo << endl;
+		cout << "Fecha: " << Aux->fecha << endl;
+		cout << "Co'digo Electoral:  " << Aux->codelec << endl;
+		cout << "Nu'mero Junta: " << Aux->numjun << endl;
+		Aux = Aux->Siguiente;
+		Contador++;
+	}
+}
+PtrTArticulo BuscarArticulo(PtrTArticulo &Lista, char cual[10])
+{
+	bool encontro = false;
+	PtrTArticulo Aux;
+	Aux = Lista;
+
+	while (encontro != true && Aux != NULL)
+		if (Aux->cedula == cual)
+			encontro = true;
+		else
+			Aux = Aux->Siguiente;
+	return Aux;
+}
+
+/*
+void GuardarInventario(PtrTArticulo Lista){
+	FILE *archivo;
+	fopen_s(&archivo, "ARCHIVO.txt", "w+");
+	if (NULL == archivo){
+		printf("No se pudo abrir el archivo.");
+	}
+	else{
+		PtrTArticulo AUX = Lista;
+		while (AUX != NULL){
+			fprintf(archivo, "%i\n", AUX->Codigo);
+			fprintf(archivo, "%s\n", AUX->Nombre);
+			fprintf(archivo, "%i\n", AUX->Disponible);
+			fprintf(archivo, "%f\n", AUX->Precio);
+			AUX = AUX->Siguiente;
+		}
+
+
+	}
+	fclose(archivo);
+}
+*/
+
+/*
+void CargarInventario(PtrTArticulo &Lista){
+	PtrTArticulo Nuevo;
+	FILE *archivo;
+	fopen_s(&archivo, "ARCHIVO.txt", "r");
+	if (NULL == archivo){
+		printf("No se pudo abrir el archivo");
+	}
+	else{
+
+
+		while (!feof(archivo)){
+			Nuevo = new(TArticulo);
+			char Cadena[20];
+			fscanf_s(archivo, "%i\n", &Nuevo->Codigo);
+			fscanf_s(archivo, "%s\n", Cadena, 20);
+			fscanf_s(archivo, "%i\n", &Nuevo->Disponible);
+			fscanf_s(archivo, "%f\n", &Nuevo->Precio);
+			strcpy_s(Nuevo->Nombre, Cadena);
+			Nuevo->Siguiente = NULL;
+			AgregarFinalInventario(Lista, Nuevo);
+
+		}
+	}
+	fclose(archivo);
+}
+*/
+
+
+
+/*
+void main(int argc, char* argv[])
+{
+	//************************************************************************
+	PtrTArticulo Llantas;
+	PtrTArticulo Abarrotes;
+	PtrTArticulo Nuevo;
+	PtrTArticulo Prueba;
+
+	//************************************************************************	
+	InicializarInventario(Abarrotes);
+
+	for (int i = 100; i <= 200; i++)
+	{
+		Nuevo = CrearArticulo(i, i, i);
+		AgregarInicioInventario(Abarrotes, Nuevo);
+	}
+	GuardarInventario(Abarrotes);
+	ListarInventario(Abarrotes);
+	DestruirInventario(Abarrotes);
+	system("cls");
+	//*************************PRUEBA DE CARGAR********************************
+	InicializarInventario(Prueba);
+	CargarInventario(Prueba);
+	ListarInventario(Prueba);
+	system("pause");
+	DestruirInventario(Prueba);
+	//************************************************************************
+	InicializarInventario(Abarrotes);
+	for (i = 200; i <= 300; i++)
+	{
+		Nuevo = CrearArticulo(i, i, i);
+		AgregarFinalInventario(Abarrotes, Nuevo);
+	}
+	ListarInventario(Abarrotes);
+	DestruirInventario(Abarrotes);
+
+
+
+
+	//************************************************************************	
+	InicializarInventario(Llantas);
+	for (i = 100; i <= 200; i++)
+	{
+		Nuevo = CrearArticulo(i, i, i);
+		AgregarInicioInventario(Llantas, Nuevo);
+	}
+	ListarInventario(Llantas);
+	DestruirInventario(Llantas);
+
+	//************************************************************************
+	InicializarInventario(Llantas);
+	for (i = 200; i <= 300; i++)
+	{
+		Nuevo = CrearArticulo(i, i, i);
+		AgregarFinalInventario(Llantas, Nuevo);
+	}
+	ListarInventario(Llantas);
+	DestruirInventario(Llantas);
+
+
+}
+*/
---
+++ 1B/proyecto 1B/proyecto 1B.cpp
@@ -0,0 +1,142 @@
+﻿// proyecto 1B.cpp
+// 
+// Para este proyecto se tiene la intención de construir
+// un comparador de rendimiento entre las diferentes estructuras
+// de datos vistas en clase; lista enlazada, árboles binarios de búsqueda, árboles AVL y árboles BTREE.
+// 
+// Realizado por:
+// Marco Angulo Elizondo: 2023367218 
+// Alana Calvo Bolaños: 2022040915
+// Esteban Cortés Mora: 2023194497
+
+
+#include <iostream>;
+#include <chrono>
+#include "AVL.h";
+#include "BTREE.h";
+#include "ListaSimple.h";
+#include "cargaPadron.h";
+
+
+
+
+using namespace std;
+
+
+typedef struct T_Votante {     char cedula[10];    char codelec[7];    char sexo;    char fecha[9];    char numjun[6];    char nombre[31];    char papellido[27];    char sapellido[27];    T_Votante * PtrSiguiente;}*PtrT_Votante;
+
+
+
+//tiempos de carga
+int tiempoDeCargaPadron;
+
+int tiempoDeListadoLE;
+int tiempoDeListadoABB;
+int tiempoDeListadoAVL;
+int tiempoDeListadoBTREE;
+
+
+
+
+int main()
+{
+    int option;
+    int option1;
+    char cedula[10];
+
+    
+    
+    
+    
+    cout << "Proyecto 1B" << endl;
+    cout << "Comparador de rendimiento de estructuras de datos\nInterfaz gráfica para la ultima entrega." << endl;
+    
+    cout << "Seleccione una opción: " << endl;
+    cout << " 1) cargar datos" << endl;
+    cout << " 2) salir" << endl;
+    cin >> option1;
+
+    switch (option1)
+    {
+    case 1: 
+        PtrT_Votante listMaestra;
+        //inicializarvotantes(listMaestra);
+        //cargarDatos(listMaestra);
+        
+
+        
+        break;
+    case 2:
+        break;
+    default:
+        break;
+    }
+
+    while (true) {
+        cout << "Seleccione una opción:" << endl;
+        cout << " 1) listar" << endl;
+        cout << " 2) búsqueda" << endl;
+        cout << " 3) liberar memoria" << endl;
+        cout << " 4) reporte de tiempos" << endl;
+        cout << " 5) salir" << endl;
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            cout << "Listado" << endl;
+            //listar();
+            break;
+        case 2:
+            cout << "Búsqueda" << endl;
+            cout << "Ingrese el número de cédula" << endl;
+            cin >> cedula;
+            //busqueda(cedula);
+            break;
+        case 3:
+            cout << "Liberando memoria..." << endl;
+            //liberarMem();
+            break;
+        case 4:
+            cout << "Reporte" << endl;
+            //reporte();
+            break;
+        case 5:
+            cout << "Saliendo..." << endl;
+            //liberarMem();
+            return 0;
+            break;
+        default:
+            cout << "opción no disponible." << endl;
+            break;
+        }
+    }
+
+
+}
+
+void cargarDatos() {
+    //Aquí se deben cargar los datos en la lista maestra, luego cargar la lista en todas las estructuras de datos.
+
+    //falta ABB
+
+    //AVL
+
+    //BTREE
+}
+
+void listar() {
+    cout << "" << endl;
+    //duda*** de deben listar todos los millones de votantes con cada estructura??
+}
+
+void busqueda(char cedula[10]) {//se recibe cedula y se busca con cada estructura
+    cout << "" << endl;
+}
+
+void liberarMem() {// usar la funcion de liberar memoria con cada una de las estructuras
+    cout << "" << endl;
+}
+
+void reporte() { // generar el reporte a partir de las variables que deben almacenar los tiempos de cada ejecución.
+    cout << "" << endl;
+}
